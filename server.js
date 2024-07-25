11b28957db37685808f193c51f8bedae74d392ef
suryauday prakash mishra <64128164+SURYAUDAY@users.noreply.github.com>
2024-07-25 20:44:36 +0530

    cors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOptions = {
